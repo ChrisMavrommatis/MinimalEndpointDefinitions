22a5fa0f26d79de1315bc2c4103234047fa5ffc7
Chris Mavrommatis <56303004+ChrisMavrommatis@users.noreply.github.com>
2024-07-24 20:41:14 +0300

    initial commit

---
+++ src/MinimalEndpointDefinitions/IEndpointDefinition.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace ChrisMavrommatis.MinimalEndpointDefinitions;
+
+public interface IEndpointDefinition
+{
+    void DefineEndpoint(IEndpointRouteBuilder endpoints);
+}
---
+++ src/MinimalEndpointDefinitions/IPartOfGroup.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace ChrisMavrommatis.MinimalEndpointDefinitions;
+
+public interface IPartOfGroup
+{
+    void DefineEndpoint(RouteGroupBuilder group);
+}
